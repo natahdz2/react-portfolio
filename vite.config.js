0f0f722c70a10a5913c425b1b0033d5f65517068
natahdz2 <hdznatahdz3@gmail.com>
2025-04-12 13:22:33 -0400

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio/', // 👈 cambia esto por el nombre de tu repo en GitHub
+  base: 'https://natahdz2.github.io/react-portfolio/',
 })
